9d275a3853240eeb9d99bbb2832a200793457422
dr-davo <david@plusplusminus.co.za>
2014-10-20 14:13:29 +0200

    monday morning

--- wp-config.php
+++ wp-config.php
@@ -25,7 +25,7 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
-define('DB_HOST', '192.168.2.196');
+define('DB_HOST', '192.168.2.102');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
